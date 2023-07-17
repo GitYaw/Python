117cccd1ef0d0507432db618a456d58b493b333c
Yaw <118967566+GitYaw@users.noreply.github.com>
2023-07-16 17:58:01 -0700

    Initial commit

---
+++ programmer.py
@@ -0,0 +1,5 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtGui import QCursor

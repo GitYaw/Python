54e4f2c0a79e3cd6955507e85dadfede38f8b7dd
Yaw <118967566+GitYaw@users.noreply.github.com>
2023-07-16 18:01:45 -0700

    Create window object

--- programmer.py
+++ programmer.py
@@ -3,3 +3,12 @@ from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWid
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QCursor
+
+app = QApplication(sys.argv)
+window = QWidget()
+window.setWindowTitle("Who wants to be a programmer???")
+window.setFixedWidth(1000)
+window.setStyleSheet("background: #161219;")
+
+window.show()
+sys.exit(app.exec())

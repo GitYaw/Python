44129f680df2c319da62a5856c3f098e16b52a37
Yaw <118967566+GitYaw@users.noreply.github.com>
2023-08-15 16:37:16 -0700

    Create button widget

--- programmer.py
+++ programmer.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QCursor
 
+
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle("Who wants to be a programmer???")
@@ -20,7 +21,21 @@ logo.setPixmap(image)
 logo.setAlignment(QtCore.Qt.AlignCenter)
 logo.setStyleSheet("margin-top: 100px;")
 
+# button widget
+button = QPushButton("PLAY")
+button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+button.setStyleSheet(
+	"*{border: 4px solid '#BC006C'; " +
+	"border-radius: 45px; " +
+	"font-size: 35px; " +
+	"color: 'white'; " +
+	"padding: 25px 0px; " +
+	"margin: 100px 200px;} " +
+	"*:hover{background: '#BC006C';}"
+)
+
 grid.addWidget(logo, 0, 0)
+grid.addWidget(button, 1, 0)
 
 
 window.setLayout(grid)

938debcc0a9c582a6423d3b4eebf02377faf7447
Yaw <118967566+GitYaw@users.noreply.github.com>
2023-08-15 16:51:25 -0700

    Create frame function

--- programmer.py
+++ programmer.py
@@ -4,6 +4,10 @@ from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QCursor
 
+widgets = {
+	"logo": [],
+	"button": []
+}
 
 app = QApplication(sys.argv)
 window = QWidget()
@@ -14,29 +18,33 @@ window.setStyleSheet("background: #161219;")
 
 grid = QGridLayout()
 
-# display logo
-image = QPixmap("logo.png")
-logo = QLabel()
-logo.setPixmap(image)
-logo.setAlignment(QtCore.Qt.AlignCenter)
-logo.setStyleSheet("margin-top: 100px;")
+def frame1():
+	# display logo
+	image = QPixmap("logo.png")
+	logo = QLabel()
+	logo.setPixmap(image)
+	logo.setAlignment(QtCore.Qt.AlignCenter)
+	logo.setStyleSheet("margin-top: 100px;")
+	widgets["logo"].append(logo)
 
-# button widget
-button = QPushButton("PLAY")
-button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-button.setStyleSheet(
-	"*{border: 4px solid '#BC006C'; " +
-	"border-radius: 45px; " +
-	"font-size: 35px; " +
-	"color: 'white'; " +
-	"padding: 25px 0px; " +
-	"margin: 100px 200px;} " +
-	"*:hover{background: '#BC006C';}"
-)
+	# button widget
+	button = QPushButton("PLAY")
+	button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+	button.setStyleSheet(
+		"*{border: 4px solid '#BC006C'; " +
+		"border-radius: 45px; " +
+		"font-size: 35px; " +
+		"color: 'white'; " +
+		"padding: 25px 0px; " +
+		"margin: 100px 200px;} " +
+		"*:hover{background: '#BC006C';}"
+	)
+	widgets["button"].append(button)
 
-grid.addWidget(logo, 0, 0)
-grid.addWidget(button, 1, 0)
+	grid.addWidget(widgets["logo"][-1], 0, 0)
+	grid.addWidget(widgets["button"][-1], 1, 0)
 
+frame1()
 
 window.setLayout(grid)
 
